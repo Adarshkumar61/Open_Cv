bef355495d48c903c7e7d80894b3d9b9a2a4a386
Adarshkumar61 <adarshb2k61@gmail.com>
2025-06-29 20:17:54 +0530

    updated

--- motion_detection.py
+++ motion_detection.py
@@ -11,3 +11,4 @@ while True:
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
