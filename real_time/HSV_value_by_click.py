b535f78a96508af4b650d9a850b5f3866e9581dd
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-02 19:48:43 +0530

    updated

--- real_time/HSV_value_by_click.py
+++ real_time/HSV_value_by_click.py
@@ -5,3 +5,19 @@ def pick_color(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         pixel = hsv[y, x]
         print(f'HSV: {pixel}')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    cv2.imshow("Frame", frame)
+    cv2.setMouseCallback("Frame", pick_color)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
