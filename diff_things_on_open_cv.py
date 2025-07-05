17a30dfaa5f5693561961de5328e4f0106d53757
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-05 07:53:08 +0530

    updated

--- diff_things_on_open_cv.py
+++ diff_things_on_open_cv.py
@@ -308,3 +308,30 @@ import numpy as np
 
 # cap.release()
 # cv2.destroyAllWindows()
+
+import numpy as np
+import cv2 as cv
+
+cam= cv.VideoCapture(0)
+while True:
+    
+   ret, frame = cam.read()
+   gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+ 
+   gray = np.float32(gray)
+   dst = cv.cornerHarris(gray,2,3,0.04)
+ 
+#result is dilated for marking the corners, not important
+   dst = cv.dilate(dst,None)
+ 
+# Threshold for an optimal value, it may vary depending on the image.
+   frame[dst>0.01*dst.max()]=[0,0,255]
+ 
+   cv.imshow('dst',frame)
+
+   if cv.waitKey(1) & 0xff == ord('q'):
+       break
+   
+cam.release()
+cv.destroyAllWindows()
+    
