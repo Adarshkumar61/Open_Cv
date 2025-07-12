cb167ad483980f08b35a7c71ebd493b6924276b4
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-12 09:40:09 +0530

    updated

--- image_operations/load_and_display_image.py
+++ image_operations/load_and_display_image.py
@@ -5,10 +5,18 @@ import cv2 as cv
 # cv.waitKey(0)
 # cv.destroyAllWindows()
 # OR
-image = cv.imread('c:\Users\adars\Pictures\robo.jpg')
+image = cv.imread('c:/Users/adars\Pictures/robo.jpg')
 # explanation:
 # there are many ways to read imagee
-# 1. backslash: use backslash n place of every forward slash
-cv.imshow('img', image)
-cv.waitKey(0)
-cv.destroyAllWindows()
+# 1. backslash: use forward slash
+# 2. use double backslash:
+# image = cv.imread('c:\\Users\\adars\Pictures\\robo.jpg')
+# 3. use a raw string:
+# image = cv.imread(r'c:/Users/adars\Pictures/robo.jpg')
+if image is None:
+    print('check path of the image'.title())
+else:
+    
+    cv.imshow('img', image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
