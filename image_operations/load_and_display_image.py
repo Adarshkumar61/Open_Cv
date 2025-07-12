f3cb4bfb3255d1e1138098c3a316d1e3496888f8
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-12 09:41:07 +0530

    updated

--- image_operations/load_and_display_image.py
+++ image_operations/load_and_display_image.py
@@ -1,10 +1,5 @@
 import cv2 as cv
-# img_path = 'c:\Users\adars\Pictures\robo.jpg'
-# img = cv.imread(img_path)
-# cv.imshow('img', img)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
-# OR
+
 image = cv.imread('c:/Users/adars\Pictures/robo.jpg')
 # explanation:
 # there are many ways to read imagee
