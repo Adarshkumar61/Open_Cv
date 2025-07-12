29e373909d2ef4b81a5380b67340d8576a7d6bcd
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-12 09:34:22 +0530

    updated

--- image_operations/load_and_display_image.py
+++ image_operations/load_and_display_image.py
@@ -7,7 +7,7 @@ import cv2 as cv
 # OR
 image = cv.imread('c:\Users\adars\Pictures\robo.jpg')
 # explanation:
-# there are many ways to display imagee
+# there are many ways to read imagee
 # 1. backslash: use backslash n place of every forward slash
 cv.imshow('img', image)
 cv.waitKey(0)
