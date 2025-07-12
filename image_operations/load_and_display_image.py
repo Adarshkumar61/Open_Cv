801bf1b0cd9aed25ccb13ef57563d874e9ab5e97
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-12 09:42:49 +0530

    updated

--- image_operations/load_and_display_image.py
+++ image_operations/load_and_display_image.py
@@ -1,13 +1,15 @@
 import cv2 as cv
 
-image = cv.imread('c:/Users/adars\Pictures/robo.jpg')
-# explanation:
 # there are many ways to read imagee
 # 1. backslash: use forward slash
+image = cv.imread('c:/Users/adars\Pictures/robo.jpg')
+
 # 2. use double backslash:
 # image = cv.imread('c:\\Users\\adars\Pictures\\robo.jpg')
+
 # 3. use a raw string:
 # image = cv.imread(r'c:/Users/adars\Pictures/robo.jpg')
+
 if image is None:
     print('check path of the image'.title())
 else:
