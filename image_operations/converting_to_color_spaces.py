4861c4bf69fca1b99c570fa75df069e6888f87f8
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-12 10:58:16 +0530

    updated

--- image_operations/converting_to_color_spaces.py
+++ image_operations/converting_to_color_spaces.py
@@ -9,12 +9,18 @@ resized = cv.resize(image, (980, 950))
 hsv = cv.cvtColor(resized, cv.COLOR_BGR2HSV)
 
 #applying Lab :
+lab = cv.cvtColor(resized, cv.COLOR_BGR2LAB)
 
+#applying blur :
+# for better contour and edge detection : reduce noise from image:
+blur = cv.GaussianBlur(resized, (5,5), 0)
 if image is None:
     print('check image source')
 else:
     # cv.imshow('gray', gray)
     # cv.imshow('resized', resized)
-    cv.imshow('hsv', hsv)
+    # cv.imshow('hsv', hsv)
+    # cv.imshow('lab', lab)
+    cv.imshow('blur', blur)
     cv.waitKey(0)
     cv.destroyAllWindows()
