5ae98158342246dca62fdeb1dfe86f2b981398b2
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-17 08:45:58 +0530

    updated

--- image_operations/differen_parameters_on_image.py
+++ image_operations/differen_parameters_on_image.py
@@ -6,21 +6,33 @@ resized = cv.resize(image, (980, 950))
 # gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
 
 # applying HSV color:
-hsv = cv.cvtColor(resized, cv.COLOR_BGR2HSV)
+# hsv = cv.cvtColor(resized, cv.COLOR_BGR2HSV)
 
 #applying Lab :
-lab = cv.cvtColor(resized, cv.COLOR_BGR2LAB)
+# lab = cv.cvtColor(resized, cv.COLOR_BGR2LAB)
 
 #applying blur :
 # for better contour and edge detection : reduce noise from image:
-blur = cv.GaussianBlur(resized, (5,5), 0)
+# blur = cv.GaussianBlur(resized, (5,5), 0)
+
+#threshholding:
+# _, thresh = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+
+#edge detection:
+# edge = cv.Canny(blur, 50, 150)
+
+approx = cv.approxPolyDP(cnt, 0.04 * cv2.arcLength(cnt, True), True)
+
 if image is None:
     print('check image source')
 else:
+    # cv.imshow('image', image)
+    cv.imshow('resized', resized)
     # cv.imshow('gray', gray)
-    # cv.imshow('resized', resized)
     # cv.imshow('hsv', hsv)
     # cv.imshow('lab', lab)
-    cv.imshow('blur', blur)
+    # cv.imshow('blur', blur)
+    # cv.imshow('thresh', thresh)
+    # cv.imshow('edge', edge)
     cv.waitKey(0)
     cv.destroyAllWindows()
