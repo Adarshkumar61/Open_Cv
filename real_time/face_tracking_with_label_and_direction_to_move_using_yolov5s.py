fb6b55e6bedf30befed75fc19f0a5b2ca4a75aa1
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-03 07:53:13 +0530

    updated

--- real_time/face_tracking_with_label_and_direction_to_move_using_yolov5s.py
+++ real_time/face_tracking_with_label_and_direction_to_move_using_yolov5s.py
@@ -2,3 +2,18 @@ import numpy as np
 import cv2 as cv
 import torch as t
 
+cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+model = t.hub.load('ultralytics/yolov5', 'yolov5s')
+
+cam = cv.VideoCapture(0)
+while True:
+    ret, frame = cv.flip(frame, 1)
+    if not ret:
+        print('frame is not capturing')
+        break
+    result = model(frame)
+    label = result.pandas().xyxy[0]['name'].tolist()
+    final = np.squeeze(result.render())
+    
+    #code for face detection and direction logic implementation:
+    
