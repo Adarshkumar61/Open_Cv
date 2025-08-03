bf037d5c6de4600247570c3f0538bce25f4716a4
Adarshkumar61 <adarshb2k61@gmail.com>
2025-08-03 07:50:12 +0530

    updated

--- real_time/real_time_object_detection_with_label.py
+++ real_time/real_time_object_detection_with_label.py
@@ -1,24 +1,23 @@
-import cv2
 import numpy as np
 import torch
+import cv2 as cv
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
-cam = cv2.VideoCapture(0)
+cam = cv.VideoCapture(0)
 while True:
     ret, frame = cam.read()
     if not ret:
         print('frame not capturing')
         break
-    # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    result = model(frame)
     
+    result = model(frame)
     label = result.pandas().xyxy[0]['name'].tolist()
     
     final = np.squeeze(result.render())
-    cv2.imshow('YOLO5', final)
-    if cv2.waitKey(1) == ord('q'):
+    cv.imshow('frame', final)
+    
+    if cv.waitKey(1) == ord('b'):
         break
-
 cam.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
