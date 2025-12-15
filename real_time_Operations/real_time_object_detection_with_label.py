83c60ffe5f42ec0ae0f8cf770059e34637945a90
Adarshkumar61 <adarshb2k61@gmail.com>
2025-12-15 10:07:51 +0530

    updated

--- real_time_Operations/real_time_object_detection_with_label.py
+++ real_time_Operations/real_time_object_detection_with_label.py
@@ -23,26 +23,3 @@
 # cv.destroyAllWindows()
 
 
-import torch
-import cv2
-import numpy as np
-
-model = torch.hub.load('./yolov5', 'yolov5s', source='local')
-
-cam = cv2.VideoCapture(0)
-
-while True:
-    ret, frame = cam.read()
-    if not ret:
-        break
-
-    results = model(frame)
-    final = np.squeeze(results.render())
-
-    cv2.imshow("YOLOv5", final)
-
-    if cv2.waitKey(1) & 0xFF == ord('b'):
-        break
-
-cam.release()
-cv2.destroyAllWindows()
