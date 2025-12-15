80212085a1d0eebd14a942f6ecac711c0328c128
Adarshkumar61 <adarshb2k61@gmail.com>
2025-12-15 10:07:17 +0530

    updated

--- real_time_Operations/real_time_object_detection_with_label.py
+++ real_time_Operations/real_time_object_detection_with_label.py
@@ -1,23 +1,48 @@
-import numpy as np
+# import numpy as np
+# import torch
+# import cv2 as cv
+
+# model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+# cam = cv.VideoCapture(0)
+# while True:
+#     ret, frame = cam.read()
+#     if not ret:
+#         print('frame not capturing')
+#         break
+    
+#     result = model(frame)
+#     label = result.pandas().xyxy[0]['name'].tolist()
+    
+#     final = np.squeeze(result.render())
+#     cv.imshow('frame', final)
+    
+#     if cv.waitKey(1) == ord('b'):
+#         break
+# cam.release()
+# cv.destroyAllWindows()
+
+
 import torch
-import cv2 as cv
+import cv2
+import numpy as np
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+model = torch.hub.load('./yolov5', 'yolov5s', source='local')
 
-cam = cv.VideoCapture(0)
+cam = cv2.VideoCapture(0)
+
 while True:
     ret, frame = cam.read()
     if not ret:
-        print('frame not capturing')
         break
-    
-    result = model(frame)
-    label = result.pandas().xyxy[0]['name'].tolist()
-    
-    final = np.squeeze(result.render())
-    cv.imshow('frame', final)
-    
-    if cv.waitKey(1) == ord('b'):
+
+    results = model(frame)
+    final = np.squeeze(results.render())
+
+    cv2.imshow("YOLOv5", final)
+
+    if cv2.waitKey(1) & 0xFF == ord('b'):
         break
+
 cam.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
